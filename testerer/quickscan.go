889f72a3105942a5e96f07048223086b633a7d8e
tob-ss <tobaodumosu1@gmail.com>
2026-02-19 14:57:11 +0000

    fix: added 10 second sleep

--- testerer/quickscan.go
+++ testerer/quickscan.go
@@ -14,6 +14,7 @@ import (
 
 func quickScan(device string, userID int) {
 	for {
+		time.Sleep(10 * time.Second)
 		ctx, cancel := context.WithTimeout(context.Background(), 30 * time.Second)
 		defer cancel()
 		client := graphql.NewClient("http://localhost:8080/query", http.DefaultClient)
