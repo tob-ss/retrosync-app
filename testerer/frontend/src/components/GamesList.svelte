b6ca676d3d286d33e84ef74d5ab75ecda86f7a2d
tob-ss <tobaodumosu1@gmail.com>
2026-02-21 03:03:07 +0000

    Update GamesList.svelte

--- testerer/frontend/src/components/GamesList.svelte
+++ testerer/frontend/src/components/GamesList.svelte
@@ -30,7 +30,7 @@
 
 
 <div class="h-full w-full">
-    <div class="h-full grid grid-cols-3 grid-rows-6 gap-4">
+    <div class="h-full grid grid-cols-3 grid-rows-6 gap-4 text-[#D7D6FC] font-heebo">
         <div class="col-span-3 row-span-6">
         {#await GetSaves()}
             <p>Getting Saves...</p>
@@ -42,6 +42,5 @@
 </div>
 
 <style>
-
 
 </style>
