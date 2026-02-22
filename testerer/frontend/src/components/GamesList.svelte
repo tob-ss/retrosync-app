05983278b7ef6067fc5836cdcc498b7dbcc4ec26
tob-ss <tobaodumosu1@gmail.com>
2026-02-22 23:30:33 +0000

    Update GamesList.svelte

--- testerer/frontend/src/components/GamesList.svelte
+++ testerer/frontend/src/components/GamesList.svelte
@@ -29,17 +29,24 @@
     
 </script>
 
-{#await GetSaves()}
-<p>Getting Saves...</p>
-{:then data}
-    {#await GetHeaders(data)}
-    <p>Getting Headers...</p>
-    {:then headers}
-    {#each headers as header}
-        <h1>{header}</h1>
-        {#each data as save}
-        <p>Showing game: {save.Game_Name}</p>
-        {/each}
-    {/each}
-    {/await}
-{/await}
+
+<div class="h-full w-full">
+    <div class="h-full grid grid-cols-3 grid-rows-6 gap-4 text-[#D7D6FC] font-heebo">
+        <div class="table col-span-3 row-span-6">
+        {#await GetSaves()}
+        <p>Getting Saves...</p>
+        {:then data}
+            {#await GetHeaders(data)}
+            <p>Getting Headers...</p>
+            {:then headers}
+            {#each headers as header}
+                <h1>{header}</h1>
+                {#each data as save}
+                <p>Showing game: {save.Game_Name}</p>
+                {/each}
+            {/each}
+            {/await}
+        {/await}
+        </div>
+    </div>
+</div>
