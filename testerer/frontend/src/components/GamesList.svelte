76e3babb86b7218617d8ff567f48faada7bcac53
tob-ss <tobaodumosu1@gmail.com>
2026-02-23 02:00:14 +0000

    fix: changed the layout for dynamic divs

--- testerer/frontend/src/components/GamesList.svelte
+++ testerer/frontend/src/components/GamesList.svelte
@@ -35,22 +35,28 @@
 
 
 <div class="h-full w-full">
-    <div class="h-full grid grid-cols-3 grid-rows-6 gap-4 text-[#D7D6FC] font-heebo">
+    
         {#await GetSaves()}
             <p>Getting Saves...</p>
         {:then data}
         {#await GetHeaders(data)}
             <p>Getting Headers...</p>
         {:then headers}
+        <div class="h-full grid grid-cols-3 grid-rows-6 gap-4 text-[#D7D6FC] font-heebo">
         {#each headers as header}
+            <div class="col-span-3">
             <h1>{header}</h1>
-            {#each data as save}
-            {#if header === save.Date_String}
-            <p>Showing game: {save.Game_Name}</p>
-            {/if}
-            {/each}
+                <div class="col-span-3 row-span-5 row-start-2">
+                {#each data as save}
+                {#if header === save.Date_String}
+                <p>Showing game: {save.Game_Name}</p>
+                {/if}
+                {/each}
+                </div>
+            </div>
         {/each}
+        </div>
         {/await}
         {/await}
-    </div>
+    
 </div>
